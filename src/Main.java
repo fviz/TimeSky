c929128813c595e0890d97bc5e75fceeb153ce81
Francisco Vizentin <franciscovizentin@gmail.com>
2019-05-01 23:44:54 +0200

    Created MongoConnect class for handling database things.

--- src/Main.java
+++ src/Main.java
@@ -1,93 +1,40 @@
-import com.mongodb.Block;
-import com.mongodb.client.MongoCollection;
-import org.bson.Document;
 import processing.core.PApplet;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoDatabase;
-
-import java.util.Map;
 import java.util.ArrayList;
 
 public class Main extends PApplet {
 
-    MongoClient mongoClient=MongoClients.create();
-    MongoDatabase database=mongoClient.getDatabase("cities");
-    MongoCollection<Document> coll=database.getCollection("cities");
+    float minimumLatitude;
+    float minimumLongitude;
+    float maximumLatitude;
+    float maximumLongitude;
 
-    ArrayList<City> cities = new ArrayList<City>();
-
-    private float minimumLatitude;
-    private float minimumLongitude;
-    private float maximumLatitude;
-    private float maximumLongitude;
-
-    Block<Document> parseBlock = new Block<Document>() {
-        @Override
-        public void apply(final Document document) {
-            City newCity = new City();
-
-            // Read data from MongoDB
-            newCity.name = document.getString("name");
-            newCity.country = document.getString("country");
-            String latitude = document.getString("latitude");
-            String longitude = document.getString("longitude");
-            Document years = (Document) document.get("years");
-
-            // Cleaning the string from any non-printable characters to avoid Java complaints
-            String cleanLatitude = latitude.replaceAll("\\P{Print}", "");
-            String cleanLongitude = longitude.replaceAll("\\P{Print}", "");
-            newCity.latitude = Float.parseFloat(cleanLatitude);
-            newCity.longitude = Float.parseFloat(cleanLongitude);
-
-            // Process years object
-            for (Map.Entry<String, Object> entry : years.entrySet()) {
-
-                String[] rawYear = entry.getKey().split("_");
-                int population = Integer.parseInt((String) entry.getValue());
-                String beforeAfter = rawYear[0];
-                int year = Integer.parseInt(rawYear[1]);
-
-                if (beforeAfter.equals("BC")) {
-                    year = -year;
-                }
-                newCity.addYears(year, population);
-            }
-
-            maximumLatitude = max(maximumLatitude, newCity.latitude);
-            maximumLongitude = max(maximumLongitude, newCity.longitude);
-            minimumLatitude = min(minimumLatitude, newCity.latitude);
-            minimumLongitude = min(minimumLongitude, newCity.longitude);
-
-            cities.add(newCity);
-
-        }
-    };
+    private ArrayList<City> cities = new ArrayList<>();
+    private MongoConnect.MongoPipe mongoPipe;
+    {
+        mongoPipe = new MongoConnect.MongoPipe(this);
+    }
 
     public static void main(String[] args) {
         PApplet.main("Main");
     }
 
     public void settings() {
-        size(1000, 700);
-        pixelDensity(2);
-    }
-
-    public void setup() {
-        background(0);
 
         maximumLatitude = MIN_FLOAT;
         maximumLongitude = MIN_FLOAT;
         minimumLatitude = MAX_FLOAT;
         minimumLongitude = MAX_FLOAT;
 
-        coll.find().forEach(parseBlock);
+        size(1000, 700);
+        pixelDensity(2);
+    }
 
-        println(minimumLatitude, maximumLatitude);
-        println(minimumLongitude, maximumLongitude);
+    public void setup() {
+
+        background(0);
+
+        mongoPipe.connect();
+        cities = mongoPipe.run();
 
         Keyframe newKeyframe = new Keyframe(2, "spawn");
     }
---
+++ src/MongoConnect.java
@@ -0,0 +1,112 @@
+import com.mongodb.Block;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+import processing.core.PApplet;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import static processing.core.PApplet.max;
+import static processing.core.PApplet.min;
+
+public class MongoConnect {
+
+    static class MongoPipe {
+
+        MongoClient client;
+        MongoDatabase database;
+
+        MongoCollection<Document> collection;
+
+        ArrayList<City> cities;
+
+        private final Main main;
+
+        MongoPipe(Main mainReference) {
+            this.main = mainReference;
+            this.cities = new ArrayList<>();
+        }
+
+        void connect() {
+            this.client = MongoClients.create();
+            this.database = getDatabase("cities");
+            this.collection = getCollection("cities");
+        }
+
+        @SuppressWarnings("deprecation")
+        ArrayList<City> run() {
+            this.collection.find().forEach(parseCities);
+            return this.cities;
+        }
+
+        MongoCollection<Document> getCollection(String collectionNameInput) {
+            return database.getCollection(collectionNameInput);
+        }
+
+        MongoDatabase getDatabase(String databaseNameInput) {
+            return client.getDatabase(databaseNameInput);
+        }
+
+        float parseCoordinate(String coordinateRaw) {
+            String coordinateClean = coordinateRaw.replaceAll("\\P{Print}", "");
+            float parsedValue = Float.parseFloat(coordinateClean);
+
+            return parsedValue;
+        }
+
+        int[] parseYear(Map.Entry<String, Object> yearInput) {
+            int[] returnData = new int[2];
+
+            String[] rawYear = yearInput.getKey().split("_");
+            int population = Integer.parseInt((String) yearInput.getValue());
+            String beforeAfter = rawYear[0];
+            int year = Integer.parseInt(rawYear[1]);
+
+            if (beforeAfter.equals("BC")) {
+                year = -year;
+            }
+
+            returnData[0] = year;
+            returnData[1] = population;
+
+            return returnData;
+        }
+
+        Block<Document> parseCities = new Block<Document>() {
+            @Override public void apply(final Document document) {
+                City newCity = new City();
+
+                // Read basic data from MongoDB
+                newCity.name = document.getString("name");
+                newCity.country = document.getString("country");
+                newCity.latitude = parseCoordinate(document.getString("latitude"));
+                newCity.longitude = parseCoordinate(document.getString("longitude"));
+                Document years = (Document) document.get("years");
+
+                // Process years object
+                for (Map.Entry<String, Object> entry : years.entrySet()) {
+
+                    int[] parsedData = parseYear(entry);
+                    newCity.addYears(parsedData[0], parsedData[1]);
+                }
+
+                main.minimumLatitude = min(main.minimumLatitude, newCity.latitude);
+                main.minimumLongitude = min(main.minimumLongitude, newCity.longitude);
+                main.maximumLatitude = max(main.maximumLatitude, newCity.latitude);
+                main.maximumLongitude = max(main.maximumLongitude, newCity.longitude);
+
+                cities.add(newCity);
+
+            }
+
+        };
+
+    }
+
+}
